3036b9dfe5fcc180aa50a85250cf489ddc7c5145
[Enrique Viveros] <github.villain713@slmail.me>
2025-05-29 10:53:40 +0200

    updating README moving styling to a styles folder modifying App.jsx to render the first title componets adding first component styling RecipeTitle updating name in index

--- src/components/RecipeTitle.jsx
+++ src/components/RecipeTitle.jsx
@@ -1,5 +1,14 @@
+import styles from '../styles/RecipeTitle.module.css';
+
 const RecipeTitle = ({ title }) => {
-    return <h3>{title}</h3>
+  return <h3 className={styles.title}>{title}</h3>;
 }
 
 export default RecipeTitle
+
+/* 
+function RecipeTitle(props) {
+    return <h3>{props.title}</h3>;
+}
+export default RecipeTitle 
+*/
