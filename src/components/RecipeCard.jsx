888ab072f5ad58c5d6cc30b1162f8b9d94e9f0d6
[Enrique Viveros] <github.villain713@slmail.me>
2025-05-31 11:40:06 +0200

    displaying first recipes cards composing RecipeCard, RecipeImage, RecipeTags and Recipe Title creating a css for RecipeImage creating a css for RecipeCard updating App to map RecipeCard

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,9 +1,23 @@
-const RecipeList = ({ recipes }) => {
+import RecipeTitle from './RecipeTitle'
+import RecipeImage from './RecipeImage';
+
+
+const RecipeCard = ({ recipe }) => {
   return (
-    <div>
-      {recipes.map(recipe => 
-        <RecipeCard key={recipe.id} recipe={recipe} />
-      )}
+    <div className="recipe_card">
+      <RecipeTitle title={recipe.name} />
+      <div>
+        <p> Region: {recipe.region}</p>
+        <p> Prep: {recipe.prepTime}m, Cook: {recipe.cookTime}m, Serves: {recipe.servings}</p>
+        <p> Difficulty: {recipe.difficulty}</p>
+      </div>
+      <RecipeImage src={recipe.image} alt={recipe.name}/>
     </div>
-  )
-}
+  );
+};
+
+export default RecipeCard
+
+
+/* import RecipeTags */
+
--- src/components/RecipeImage.jsx
+++ src/components/RecipeImage.jsx
@@ -1,3 +1,8 @@
-const RecipeImage = ({ src, alt }) => {
-    return <img src={src} alt={alt}/>
+import styles from '../styles/RecipeImage.module.css';
+
+
+const RecipeImage = ({ src, alt}) => {
+    return <img src={src} alt={alt} className={styles.image}/>
 }
+
+export default RecipeImage;
